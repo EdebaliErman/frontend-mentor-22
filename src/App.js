97fd1247746fc3317f0a036ca7b64865887a6bc9
EdebaliErman <edebalierman@gmail.com>
2023-12-03 18:50:36 +0300

    Background add

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   const [data, setData] = useState(1)
 
   useEffect(() => {
-    profileFind(next,setData)
-  },[next])
+    profileFind(next, setData)
+  }, [next])
 
   const counter = () => {
     return next < 2 && setNext(next + 1)
@@ -17,17 +17,27 @@ function App() {
   const uncounter = () => {
     return next > 1 && setNext(next - 1)
   }
+
   return (
     <div className="App">
-      <Card 
-      name={data.name}
-      img={data.img}
-      job={data.job}
-      pragraph={data.paragraph}
-      key={data.id}
+      <img className='z-0 absolute left-32' src={process.env.PUBLIC_URL+"./Assets/pattern-quotes.svg"} alt=''/>
+      <Card
+        name={data.name}
+        img={data.img}
+        job={data.job}
+        pragraph={data.paragraph}
+        key={data.id}
       />
-      <button onClick={counter}>+</button>
-      <button onClick={uncounter}>-</button>
+      <div className='buttons'>
+        <button onClick={uncounter}>
+          <img src={process.env.PUBLIC_URL + "./Assets/icon-prev.svg"} alt='' />
+        </button>
+        <button onClick={counter}>
+          <img src={process.env.PUBLIC_URL + "./Assets/icon-next.svg"} alt='' />
+        </button>
+      </div>
+      <img className='z-0 absolute right-0 top-0' src={process.env.PUBLIC_URL+"./Assets/pattern-bg.svg"} alt=''/>
+      <img className='z-0 absolute left-0' src={process.env.PUBLIC_URL+"./Assets/pattern-curve.svg"} alt=''/>
     </div>
   );
 }
--- src/Components/Card.js/Card.js
+++ src/Components/Card.js/Card.js
@@ -1,21 +1,21 @@
 import React from 'react'
-
+import "../../Style/Card.css"
 function Card({ pragraph, name, job, img }) {
-    
-    const url=process.env.PUBLIC_URL+"./Assets/"
+
+    const url = process.env.PUBLIC_URL + "./Assets/"
 
     return (
-        <div>
-            <div>
-                <p>{pragraph}</p>
+        <div className='Card'>
+            <h2>"{pragraph}"</h2>
+            <div className='Card-text'>
                 <h4>
                     <span>{name}</span>
-                    {job}
+                    {" " + job}
                 </h4>
             </div>
-            <div>
-        <img src={url+img} alt=''/>
-            </div>
+
+            <img src={url + img} alt='' />
+
         </div>
     )
 }
